4d40b70502552933af08f5ca81735ca93a6f2410
Greg Mercer (GBear) <gmercer@stanford.edu>
2016-02-22 11:16:59 -0800

    WP-6623 Added new template for ee section header

---
+++ templates/gsb-feature-program-ct-ee-section-header.tpl.php
@@ -0,0 +1,23 @@
+<div class="ee-section-header">
+  <div class="ee-section-header-image">
+    <?php print $ee_section_header_image; ?>
+  </div>
+  <div>
+    <div class="ee-section-header-title">
+      <?php print $ee_section_header_title; ?>
+    </div>
+    <div class="ee-section-header-menu">
+      <?php print $ee_section_header_menu; ?>
+    </div>
+    <div class="ee-section-header-follow-us">
+      <?php print $ee_section_header_follow_us; ?>
+    </div>
+  </div>
+  <div>
+    <?php if ($ee_section_header_apply_button) : ?>
+    <div class="ee-section-header-apply-button">
+      <?php print $ee_section_header_apply_button; ?>
+    </div>
+    <?php endif; ?>
+  </div>
+</div>
