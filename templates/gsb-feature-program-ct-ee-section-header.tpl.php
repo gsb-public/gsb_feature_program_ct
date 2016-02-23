1cd5689e51c6e6c0fb3700f281e0941cd0bc26f8
Meghan Plunkett <meghanp@stanford.edu>
2016-02-23 16:17:06 -0500

    WP-6640: update template for menu block.

--- templates/gsb-feature-program-ct-ee-section-header.tpl.php
+++ templates/gsb-feature-program-ct-ee-section-header.tpl.php
@@ -2,15 +2,15 @@
   <div class="ee-section-header-image">
     <?php print $ee_section_header_image; ?>
   </div>
-  <div>
-    <div class="ee-section-header-title">
-      <?php print $ee_section_header_title; ?>
-    </div>
-    <div class="ee-section-header-menu">
-      <?php print $ee_section_header_menu; ?>
-    </div>
-    <div class="ee-section-header-follow-us">
-      <?php print $ee_section_header_follow_us; ?>
+  <div class="ee-section-header-overlay">
+    <div class="overlay-inner-wrapper">
+      <h1 class="ee-section-header-title"><?php print $ee_section_header_title; ?></h1>
+      <div class="ee-section-header-menu">
+        <?php print $ee_section_header_menu; ?>
+      </div>
+      <div class="ee-section-header-follow-us">
+        <?php print $ee_section_header_follow_us; ?>
+      </div>
     </div>
   </div>
 </div>
