1e102f6635d400a5e41d49f3e2e6eb7bc835b9d7
Greg Mercer (GBear) <gmercer@stanford.edu>
2016-02-24 07:53:03 -0800

    WP-6623 Adding a bit more empty checking for the ee section header

--- templates/gsb-feature-program-ct-ee-section-apply-brochure.tpl.php
+++ templates/gsb-feature-program-ct-ee-section-apply-brochure.tpl.php
@@ -1,6 +1,6 @@
 <div class="ee-section-apply-brochure">
   <div>
-    <?php if ($ee_section_apply_brochure_apply_button) : ?>
+    <?php if (!empty($ee_section_apply_brochure_apply_button)) : ?>
     <div class="ee-section-apply_brochure_apply-button">
       <?php print $ee_section_apply_brochure_apply_button; ?>
     </div>
