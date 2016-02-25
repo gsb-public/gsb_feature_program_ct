40bf6a3f099cbd4b5c47a37bd5279037024d66ae
Greg Mercer (GBear) <gmercer@stanford.edu>
2016-02-25 14:34:24 -0800

    Added code to render the field_faculty_directors field collection

--- templates/gsb-feature-program-ct-ee-section-header.tpl.php
+++ templates/gsb-feature-program-ct-ee-section-header.tpl.php
@@ -1,7 +1,9 @@
 <div class="ee-section-header">
-  <div class="ee-section-header-image">
-    <?php print $ee_section_header_image; ?>
-  </div>
+  <?php if (!empty($ee_section_header_image)) : ?>
+    <div class="ee-section-header-image">
+      <?php print $ee_section_header_image; ?>
+    </div>
+  <?php endif; ?>
   <div class="ee-section-header-overlay">
     <div class="overlay-inner-wrapper">
       <h1 class="ee-section-header-title"><?php print $ee_section_header_title; ?></h1>
