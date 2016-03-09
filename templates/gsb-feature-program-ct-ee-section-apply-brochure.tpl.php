6ea562e93beb85bc3e5104654dcedde0d267d95c
Greg Mercer <gmercer@stanford.edu>
2016-03-09 13:45:42 -0800

    WP-6741 Added code to display theme var ee_section_apply_brochure_brochure_download_link

--- templates/gsb-feature-program-ct-ee-section-apply-brochure.tpl.php
+++ templates/gsb-feature-program-ct-ee-section-apply-brochure.tpl.php
@@ -4,4 +4,9 @@
       <?php print $ee_section_apply_brochure_apply_button; ?>
     </div>
   <?php endif; ?>
+  <?php if (!empty($ee_section_apply_brochure_brochure_download_link)) : ?>
+    <div class="download-brochure">
+      <?php print $ee_section_apply_brochure_brochure_download_link; ?>
+    </div>
+  <?php endif; ?>
 </div>
