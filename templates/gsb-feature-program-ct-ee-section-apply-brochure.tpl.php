ad422ed3d7051dd2ec2da78b4149897e7bc74148
Greg Mercer (GBear) <gmercer@stanford.edu>
2016-03-16 15:08:12 -0700

    WP-6788 Adding new pre-qualification link before the apply button

--- templates/gsb-feature-program-ct-ee-section-apply-brochure.tpl.php
+++ templates/gsb-feature-program-ct-ee-section-apply-brochure.tpl.php
@@ -1,4 +1,9 @@
 <div class="ee-section-apply-brochure">
+  <?php if (!empty(ee_section_pre_qualification_link)) : ?>
+    <div class="pre-qualication-link">
+      <?php print $ee_section_pre_qualification_link; ?>
+    </div>
+  <?php endif; ?>
   <?php if (!empty($ee_section_apply_brochure_apply_button)) : ?>
     <div class="apply-button">
       <?php print $ee_section_apply_brochure_apply_button; ?>
