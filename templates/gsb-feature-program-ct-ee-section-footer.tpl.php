d42a9d84830df823648afd35bc667e072c43c876
Greg Mercer (GBear) <gmercer@stanford.edu>
2016-03-22 15:18:15 -0700

    WP-6796 Added new block for ee-section-footer

---
+++ templates/gsb-feature-program-ct-ee-section-footer.tpl.php
@@ -0,0 +1,13 @@
+<div class="ee-section-footer">
+    <div class="ee-section-footer-subject-to-change">
+      <?php print $ee_section_footer_subject_to_change; ?>
+    </div>
+  <div class="ee-section-footer-overlay">
+    <div class="overlay-inner-wrapper">
+      <h1 class="ee-section-footer-title"><?php print $ee_section_footer_title; ?></h1>
+      <div class="ee-section-footer-menu">
+        <?php print $ee_section_footer_menu; ?>
+      </div>
+    </div>
+  </div>
+</div>
