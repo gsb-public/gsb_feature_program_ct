dc0f1ac5b646b8a368153043e8ffa5c33fe79c38
Greg Mercer (GBear) <gmercer@stanford.edu>
2016-03-23 10:25:50 -0700

    WP-6834 Added code to recreate the connection in the PPP back to a recreated Program.

--- templates/gsb-feature-program-ct-ee-section-footer.tpl.php
+++ templates/gsb-feature-program-ct-ee-section-footer.tpl.php
@@ -1,13 +1,19 @@
 <div class="ee-section-footer">
-    <div class="ee-section-footer-subject-to-change">
-      <?php print $ee_section_footer_subject_to_change; ?>
-    </div>
+    <?php if (!empty($ee_section_footer_subject_to_change)) : ?>
+      <div class="ee-section-footer-subject-to-change">
+        <?php print $ee_section_footer_subject_to_change; ?>
+      </div>
+    <?php endif; ?>
   <div class="ee-section-footer-overlay">
     <div class="overlay-inner-wrapper">
-      <h1 class="ee-section-footer-title"><?php print $ee_section_footer_title; ?></h1>
-      <div class="ee-section-footer-menu">
-        <?php print $ee_section_footer_menu; ?>
-      </div>
+      <?php if (!empty($ee_section_footer_title)) : ?>
+        <h1 class="ee-section-footer-title"><?php print $ee_section_footer_title; ?></h1>
+      <?php endif; ?>
+      <?php if (!empty($ee_section_footer_menu)) : ?>
+        <div class="ee-section-footer-menu">
+          <?php print $ee_section_footer_menu; ?>
+        </div>
+      <?php endif; ?>
     </div>
   </div>
 </div>
--- templates/gsb-feature-program-ct-ee-section-header.tpl.php
+++ templates/gsb-feature-program-ct-ee-section-header.tpl.php
@@ -6,13 +6,19 @@
   <?php endif; ?>
   <div class="ee-section-header-overlay">
     <div class="overlay-inner-wrapper">
-      <h1 class="ee-section-header-title"><?php print $ee_section_header_title; ?></h1>
-      <div class="ee-section-header-menu">
-        <?php print $ee_section_header_menu; ?>
-      </div>
-      <div class="ee-section-header-social-buttons">
-        <?php print $ee_section_header_social_buttons; ?>
-      </div>
+      <?php if (!empty($ee_section_header_title)) : ?>
+        <h1 class="ee-section-header-title"><?php print $ee_section_header_title; ?></h1>
+      <?php endif; ?>
+      <?php if (!empty($ee_section_header_menu)) : ?>
+        <div class="ee-section-header-menu">
+          <?php print $ee_section_header_menu; ?>
+        </div>
+      <?php endif; ?>
+      <?php if (!empty($ee_section_header_social_buttons)) : ?>
+        <div class="ee-section-header-social-buttons">
+          <?php print $ee_section_header_social_buttons; ?>
+        </div>
+      <?php endif; ?>
     </div>
   </div>
 </div>
