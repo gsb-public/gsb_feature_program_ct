67a6c3425d39aaa2a4fa60c385c4a1813cb77c04
Meghan Plunkett <meghanp@stanford.edu>
2016-05-02 15:33:34 -0400

    WP-7073: change link order for responsive.

--- templates/gsb-feature-program-ct-ee-section-apply-brochure.tpl.php
+++ templates/gsb-feature-program-ct-ee-section-apply-brochure.tpl.php
@@ -1,7 +1,7 @@
 <div class="ee-section-apply-brochure">
-  <?php if (!empty($ee_section_pre_qualification_link)) : ?>
-    <div class="pre-qualication-link">
-      <?php print $ee_section_pre_qualification_link; ?>
+  <?php if (!empty($ee_section_apply_brochure_brochure_download_link)) : ?>
+    <div class="download-brochure">
+      <?php print $ee_section_apply_brochure_brochure_download_link; ?>
     </div>
   <?php endif; ?>
   <?php if (!empty($ee_section_apply_brochure_apply_button)) : ?>
@@ -9,9 +9,9 @@
       <?php print $ee_section_apply_brochure_apply_button; ?>
     </div>
   <?php endif; ?>
-  <?php if (!empty($ee_section_apply_brochure_brochure_download_link)) : ?>
-    <div class="download-brochure">
-      <?php print $ee_section_apply_brochure_brochure_download_link; ?>
+  <?php if (!empty($ee_section_pre_qualification_link)) : ?>
+    <div class="pre-qualication-link">
+      <?php print $ee_section_pre_qualification_link; ?>
     </div>
   <?php endif; ?>
 </div>
