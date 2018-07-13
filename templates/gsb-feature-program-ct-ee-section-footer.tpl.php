700a438959f882ce2f748b6f945f09911130216a
Greg Mercer <gmercer@stanford.edu>
2018-07-13 11:05:24 -0700

    WP-8048 Changing h2 to span

--- templates/gsb-feature-program-ct-ee-section-footer.tpl.php
+++ templates/gsb-feature-program-ct-ee-section-footer.tpl.php
@@ -7,7 +7,7 @@
   <div class="ee-section-footer-overlay">
     <div class="overlay-inner-wrapper">
       <?php if (!empty($ee_section_footer_title)) : ?>
-        <h2 class="ee-section-footer-title"><?php print $ee_section_footer_title; ?></h2>
+        <span class="ee-section-footer-title"><?php print $ee_section_footer_title; ?></span>
       <?php endif; ?>
       <?php if (!empty($ee_section_footer_menu)) : ?>
         <div class="ee-section-footer-menu">
